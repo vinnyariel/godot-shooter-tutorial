106686164eac185a31c65c50c83180cf6e91af73
vinnyariel <vinnyariel11@gmail.com>
2023-07-19 19:46:56 -0300

    Fade transition scene
    A simple canvas layer modulate animation to smooth transitions

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,8 +1,6 @@
 extends LevelParent
 
-@export var outside_level_scene: PackedScene
-
 func _on_exit_gate_body_entered(_body):
 	var tween = get_tree().create_tween()
 	tween.tween_property($Player, "speed", 0, 1).set_trans(Tween.TRANS_QUAD)
-	get_tree().change_scene_to_packed(outside_level_scene)
+	FadeScreen.change_scene("res://scenes/levels/outside.tscn")
